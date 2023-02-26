626e80a4de9a244de3fa54ef5618462f427fbacf
digocorbellini <rodrigoocpereira@gmail.com>
2023-02-26 04:31:59 -0600

    initialized project

---
+++ Plugins/RuntimeTerminalPlugin/Source/RuntimeTerminalPlugin/Public/TerminalWindow.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TerminalWindow.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNTIMETERMINALPLUGIN_API ATerminalWindow : public AHUD
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RuntimeTerminal/RuntimeTerminalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RuntimeTerminalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNTIMETERMINAL_API ARuntimeTerminalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
